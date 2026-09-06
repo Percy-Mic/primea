22db1d68eedcfc26feca6846b2421ab1c7021491
Percy-Mic <percymicnono@gmail.com>
2026-09-06 14:51:16 +0800

    Update page.tsx

--- app/(store)/products/page.tsx
+++ app/(store)/products/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import UserNav from '@/components/UserNav'
+/*import UserNav from '@/components/UserNav'*/
 
 interface Product {
   id: string
