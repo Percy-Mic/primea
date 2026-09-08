5d2f711ee994130f8ebdfda53578f7e4cd9c2e80
Percy-Mic <percymicnono@gmail.com>
2026-09-09 00:56:23 +0800

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -78,7 +78,7 @@ export default function AdminProfilePage() {
   const handleAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     try {
       setUploading(true)
-      if (!user || !e.target.files?[0]) return
+      if (!user || !e.target.files?.[0]) return
       const file = e.target.files[0]
       setAvatarUrl(URL.createObjectURL(file))
       const fileExt = file.name.split('.').pop()
