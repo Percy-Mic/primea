114dc72ced4eda1e7aec0ce37ab53285c494ddc6
Percy-Mic <percymicnono@gmail.com>
2026-09-09 09:05:42 +0800

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://znxaurlyjgyklqtjjivu.supabase.co'
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_tSdZdaLzS5BbaIrcB6jiEA_DCQS-X7n'
